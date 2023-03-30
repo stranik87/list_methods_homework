3344870f3386b109021860acc9c793c760dd2a3a
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-30 15:29:04 +0500

    list03

--- List03.py
+++ List03.py
@@ -7,4 +7,7 @@ def main(fruits1, fruits2):
     Returns:
         list: return answer
     """
-    return
+    fruits1.extend(fruits2)
+    return fruits1
+
+print(main(["apple", "banana"], ["kiwi", "pear"]))
